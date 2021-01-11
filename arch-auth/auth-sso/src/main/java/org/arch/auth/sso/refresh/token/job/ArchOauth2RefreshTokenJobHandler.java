d4701689629e27ace115c37358299f4ac602e81f
ZeroOrInfinity <zyw23zyw23@163.com>
2021-01-11 22:11:02 +0800

    新增: 多租户上下文处理器. 新增: 第三方登录定时任务处理器. 日志: 更新日志配置. 依赖: 添加 nacos client 依赖.

---
+++ arch-auth/auth-sso/src/main/java/org/arch/auth/sso/refresh/token/job/ArchOauth2RefreshTokenJobHandler.java
@@ -0,0 +1,34 @@
+package org.arch.auth.sso.refresh.token.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import top.dcenter.ums.security.common.api.tasks.handler.JobHandler;
+import top.dcenter.ums.security.core.oauth.properties.Auth2Properties;
+
+/**
+ * 第三方登录用户的 accessToken 与 refreshToken 刷新的定时任务.
+ * @author YongWu zheng
+ * @weixin z56133
+ * @since 2021.1.11 20:44
+ */
+@Slf4j
+@Component
+public class ArchOauth2RefreshTokenJobHandler implements JobHandler {
+
+    private final String cronExp;
+
+    public ArchOauth2RefreshTokenJobHandler(Auth2Properties auth2Properties) {
+        this.cronExp = auth2Properties.getRefreshTokenJobCron();
+    }
+
+    @Override
+    public void run() {
+        // TODO 参考 top.dcenter.ums.security.core.oauth.job.RefreshTokenJobHandler
+
+    }
+
+    @Override
+    public String cronExp() {
+        return this.cronExp;
+    }
+}
