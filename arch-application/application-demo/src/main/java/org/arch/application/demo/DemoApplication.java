7012d142d31093f0e3551be413cdb007baaba454
geeker-lait <lait.zhang@gmail.com>
2021-01-12 11:25:21 +0800

    update

---
+++ arch-application/application-demo/src/main/java/org/arch/application/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package org.arch.application.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+/**
+ * <p>
+ * Application
+ * </p>
+ *
+ * @author lait
+ * @since 2020-11-13 10:30:40
+ * @description 
+ **/
+
+@SpringBootApplication
+@MapperScan(basePackages = {"org.arch.application.demo.crud.mapper"})
+@EnableOAuth2Sso
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ arch-application/application-demo/src/main/java/org/arch/application/demo/auth/domain/UserDTO.java
@@ -0,0 +1,15 @@
+package org.arch.application.demo.auth.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String password;
+    private List<String> roles;
+}
