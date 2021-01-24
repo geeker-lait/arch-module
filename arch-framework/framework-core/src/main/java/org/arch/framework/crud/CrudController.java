b0079feb6ebdfef47a1197389dde9939c1e57bc5
ZeroOrInfinity <zyw23zyw23@163.com>
2021-01-24 23:23:12 +0800

    改进: 实现通用控制器.

--- arch-framework/framework-core/src/main/java/org/arch/framework/crud/CrudController.java
+++ arch-framework/framework-core/src/main/java/org/arch/framework/crud/CrudController.java
@@ -1,20 +1,173 @@
 package org.arch.framework.crud;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import org.arch.framework.crud.dto.BaseSearchDto;
 import org.arch.framework.ums.bean.TokenInfo;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
+import java.io.Serializable;
 import java.util.List;
+
+import static org.arch.framework.exception.constant.ResponseStatusCode.FAILED;
 
 
 /**
- * @Author lait.zhang@gmail.com
- * @Tel 15801818092
- * @Date 11/21/2019
- * @Description ${Description}
+ * 通用控制器
+ * @author lait.zhang@gmail.com
+ * @tel 15801818092
+ * @date 11/21/2019
+ * @description ${Description}
  */
-public interface CrudController<T extends CrudService> {
+public interface CrudController<T extends Model<T>, ID extends Serializable,
+        S extends BaseSearchDto, CS extends CrudService<T, ID>> {
+
+    /**
+     * 获取 {@link CrudService} 实例
+     * @return 返回 {@link CrudService} 实例
+     */
+    CS getCrudService();
+
+    /**
+     * 获取 {@link BaseSearchDto} 实例
+     * @return 返回 {@link BaseSearchDto} 实例
+     */
+    S getSearchDto();
+
+    /**
+     * 转换为 {@link BaseSearchDto} 的子类对象.
+     * @param entity 实体类
+     * @return  返回 {@link BaseSearchDto} 的子类对象.
+     */
+    default S convertSearchDto(T entity) {
+        S searchDto = getSearchDto();
+        getCrudService().mapper(entity, searchDto);
+        return searchDto;
+    }
+
+    /**
+     * 根据 token info 处理 t 后返回 t.
+     * @param token token info
+     * @param t     实体类
+     * @return  返回处理后的实体类
+     */
+    default T resolver(TokenInfo token, T t) {
+        return t;
+    }
+
+    /**
+     * 保存
+     * @param t     实体类
+     * @param token token info
+     * @return  {@link Response}
+     */
+    @PostMapping
+    default Response<T> save(@Valid @RequestBody T t, TokenInfo token) {
+        resolver(token, t);
+        getCrudService().save(t);
+        return Response.success(t);
+    }
+
+    /**
+     * 批量保存
+     * @param entityList 实体类列表
+     * @return  {@link Response}
+     */
+    @PostMapping("/saves")
+    default Response<List<T>> saveAll(@Valid @RequestBody List<T> entityList) {
+        getCrudService().saveList(entityList);
+        return Response.success(entityList);
+    }
+
+    /**
+     * 根据 id 查询对象
+     * @param id    id
+     * @return  {@link Response}
+     */
+    @GetMapping(path = "/{id}")
+    default Response<T> findById(@PathVariable("id") ID id) {
+        return Response.success(getCrudService().findById(id));
+    }
+
+    /**
+     * 根据 entity 条件查询对象
+     * @param entity    实体类
+     * @param token     token info
+     * @return  {@link Response}
+     */
+    @GetMapping("single")
+    default Response<T> findOne(T entity, TokenInfo token) {
+        try {
+            resolver(token, entity);
+            S searchDto = convertSearchDto(entity);
+            T t = getCrudService().findOneByMapParams(searchDto.getSearchParams());
+            return Response.success(t);
+        } catch (Exception e) {
+            if (e instanceof IncorrectResultSizeDataAccessException) {
+                return Response.error(FAILED.getCode(),"查询到多个结果");
+            } else {
+                return Response.error(FAILED.getCode(), e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * 根据 entity 条件查询对象列表
+     * @param t         实体类
+     * @param token     token info
+     * @return  {@link Response}
+     */
+    @GetMapping("find")
+    default Response<List<T>> find(T t, TokenInfo token) {
+        resolver(token, t);
+        S searchDto = convertSearchDto(t);
+        return Response.success(getCrudService().findAllByMapParams(searchDto.getSearchParams()));
+    }
+
+    /**
+     * 查询所有列表
+     * @param token     token info
+     * @return  {@link Response}
+     */
+    @GetMapping("list")
+    default Response<List<T>> list(TokenInfo token) {
+        resolver(token,null);
+        return Response.success(getCrudService().findAll());
+    }
+
+    /**
+     * 分页查询
+     * @param entity        实体类
+     * @param pageNumber    第几页
+     * @param pageSize      页大小
+     * @param token         token info
+     * @return  {@link Response}
+     */
+    @GetMapping("page")
+    default Response<IPage<T>> page(T entity, Integer pageNumber, Integer pageSize, TokenInfo token) {
+        resolver(token, entity);
+        S searchDto = convertSearchDto(entity);
+        return Response.success(getCrudService().findPage(searchDto.getSearchParams(), pageNumber, pageSize));
+    }
+
+    /**
+     * 根据 id 删除
+     * @param id    id
+     * @return  {@link Response}
+     */
+    @DeleteMapping(path = "/{id}")
+    default Response<Boolean> deleteById(@PathVariable("id") ID id) {
+        getCrudService().deleteById(id);
+        return Response.success(true);
+    }
+
+//public interface CrudController<T extends CrudService> {
 
 
 //    CrudService getCrudService();
