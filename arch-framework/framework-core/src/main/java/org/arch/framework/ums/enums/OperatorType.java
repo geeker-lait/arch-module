a7b57324fd0cd6c417090e8f30ba1c942edf93ce
ZeroOrInfinity <zyw23zyw23@163.com>
2021-01-29 23:04:56 +0800

    新增: account 与 user 实体相关的枚举类.

---
+++ arch-framework/framework-core/src/main/java/org/arch/framework/ums/enums/OperatorType.java
@@ -0,0 +1,26 @@
+package org.arch.framework.ums.enums;
+
+/**
+ * 操作类型
+ * @author YongWu zheng
+ * @weixin z56133
+ * @since 2021.1.29 22:54
+ */
+public enum OperatorType {
+    /**
+     * 查询
+     */
+    QUERY,
+    /**
+     * 更新
+     */
+    UPDATE,
+    /**
+     * 新增
+     */
+    ADD,
+    /**
+     * 删除
+     */
+    DELETE
+}
