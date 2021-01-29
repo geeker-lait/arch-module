5e733caa2ec5f778b32500f60a0260e50f712e9b
ZeroOrInfinity <zyw23zyw23@163.com>
2021-01-29 22:15:56 +0800

    新增: account 与 user 实体相关的枚举类.

---
+++ arch-framework/framework-core/src/main/java/org/arch/framework/ums/enums/Mno.java
@@ -0,0 +1,27 @@
+package org.arch.framework.ums.enums;
+
+/**
+ * 电话类型
+ * @author YongWu zheng
+ * @weixin z56133
+ * @since 2021.1.29 20:33
+ */
+public enum Mno {
+    /**
+     * 移动
+     */
+    CMCC,
+    /**
+     * 联通
+     */
+    CUCC,
+    /**
+     * 电信
+     */
+    CTCC,
+    /**
+     * 座机
+     */
+    LANDLINE
+
+}
