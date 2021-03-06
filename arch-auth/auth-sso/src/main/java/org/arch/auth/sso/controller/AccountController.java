00e66d8aa399693f5cf748e6715a5a7ccd7c67b4
ZeroOrInfinity <zyw23zyw23@163.com>
2021-03-06 13:20:37 +0800

    core/sso/新增: 删除账号事件, 发布删除账号事件, 删除账号监听器.

--- arch-auth/auth-sso/src/main/java/org/arch/auth/sso/controller/AccountController.java
+++ arch-auth/auth-sso/src/main/java/org/arch/auth/sso/controller/AccountController.java
@@ -3,11 +3,18 @@ package org.arch.auth.sso.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.arch.framework.beans.Response;
+import org.arch.framework.event.DeleteAccountEvent;
 import org.arch.ums.feign.account.client.UmsAccountIdentifierFeignService;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.util.Objects.nonNull;
 
 /**
  * 账号相关操作控制器
@@ -19,9 +26,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 @RequiredArgsConstructor
 @Slf4j
-public class AccountController {
+public class AccountController implements ApplicationContextAware {
 
     private final UmsAccountIdentifierFeignService umsAccountIdentifierFeignService;
+    private ApplicationContext applicationContext;
 
     /**
      * 删除账号
@@ -31,11 +39,21 @@ public class AccountController {
     @DeleteMapping("/{accountId}")
     public Response<Boolean> deleteAccount(@PathVariable("accountId") Long accountId) {
         try {
-            return umsAccountIdentifierFeignService.deleteByAccountId(accountId);
+            Response<Boolean> response = umsAccountIdentifierFeignService.deleteByAccountId(accountId);
+            Boolean successData = response.getSuccessData();
+            if (nonNull(successData) && successData) {
+            	this.applicationContext.publishEvent(new DeleteAccountEvent(accountId));
+            }
+            return response;
         }
         catch (Exception e) {
             log.error(e.getMessage(),e);
             return Response.failed(accountId + " 删除失败");
         }
+    }
+
+    @Override
+    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
---
+++ arch-auth/auth-sso/src/main/java/org/arch/auth/sso/listener/DeleteAccountListener.java
@@ -0,0 +1,23 @@
+package org.arch.auth.sso.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.arch.framework.event.DeleteAccountEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+/**
+ * 删除账号监听器
+ * @author YongWu zheng
+ * @weixin z56133
+ * @since 2021.3.6 13:17
+ */
+@Component
+@Slf4j
+public class DeleteAccountListener implements ApplicationListener<DeleteAccountEvent> {
+    @Override
+    public void onApplicationEvent(@NonNull DeleteAccountEvent event) {
+        // 记录日志
+        log.info("删除账号事件: accountId: {}", event.getAccountId());
+    }
+}
