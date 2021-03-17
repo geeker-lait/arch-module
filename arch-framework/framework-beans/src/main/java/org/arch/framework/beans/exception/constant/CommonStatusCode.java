bc4ab5e883a42f7e384e0c7c15538bee823ca105
ZeroOrInfinity <zyw23zyw23@163.com>
2021-03-17 14:41:42 +0800

    core/改进: 添加 NestedServletException 与 MaxUploadSizeExceededException 异常的处理.

--- arch-framework/framework-beans/src/main/java/org/arch/framework/beans/exception/constant/CommonStatusCode.java
+++ arch-framework/framework-beans/src/main/java/org/arch/framework/beans/exception/constant/CommonStatusCode.java
@@ -28,13 +28,30 @@ public enum CommonStatusCode implements CommonExceptionAssert {
     // retry
     DUPLICATE_KEY(7001, "主键或唯一索引重复"),
 
-    // 推荐码与账号类型
+    // 上传
+    MAX_UPLOAD_SIZE_EXCEEDED_EXCEPTION(7960, "超出最大上传尺寸"),
+
+    // response
+    RESPONSE_FAILED(7970, "响应结果为失败状态"),
+
+    // 手机归属地与号段
+    QUERY_MOBILE_SEGMENT_FAILED(7980, "查询手机号段失败"),
+    MOBILE_SEGMENT_NOT_DATA(7981, "手机号段没有数据"),
+    QUERY_MOBILE_INFO_FAILED(7982, "查询手机归属地失败"),
+    SAVES_MOBILE_INFO_PARTIAL_FAILED(7983, "批量保存手机归属地部分失败"),
+    SAVES_MOBILE_INFO_FAILED(7984, "批量保存手机归属地失败"),
+    SAVES_MOBILE_SEGMENT_PARTIAL_FAILED(7985, "批量保存手机号段部分失败"),
+    SAVES_MOBILE_SEGMENT_FAILED(7986, "批量保存手机号段失败"),
+
+    // 推荐码与账号类型,
     EXTRACT_ACCOUNT_TYPE(7990, "提取账号类型失败, aid[%s]"),
     NOT_USER_OR_ARCH_SOURCE_TYPE(7991, "推荐类型不是 USER 或 ARCH 类型"),
     EXTRACT_USER_RECOMMEND_CODE_FAILED(7992, "提取 USER 推荐码失败: code[%s]"),
     QUERY_USER_RECOMMEND_CODE_FAILED(7993, "查询 USER 推荐码失败: code[%s]"),
     GENERATE_USER_RECOMMEND_CODE_FAILED(7994, "生成用户推荐关系失败: accountId[%s]"),
     SAVE_USER_RECOMMEND_RELATION_FAILED(7995, "保存用户推荐关系失败: code[%s]"),
+
+
 
     // Time
     DATE_NOT_NULL(5001, "日期不能为空"), DATETIME_NOT_NULL(5001, "时间不能为空"), TIME_NOT_NULL(5001, "时间不能为空"),
--- arch-framework/framework-core/src/main/java/org/arch/framework/ums/filter/ErrorHandlerFilter.java
+++ arch-framework/framework-core/src/main/java/org/arch/framework/ums/filter/ErrorHandlerFilter.java
@@ -7,6 +7,8 @@ import org.arch.framework.beans.exception.constant.CommonStatusCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.util.NestedServletException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -14,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static java.util.Objects.nonNull;
 import static org.arch.framework.beans.Response.failed;
 import static top.dcenter.ums.security.common.utils.JsonUtil.responseWithJson;
 import static top.dcenter.ums.security.common.utils.JsonUtil.toJsonString;
@@ -26,6 +29,7 @@ import static top.dcenter.ums.security.common.utils.JsonUtil.toJsonString;
  */
 @Slf4j
 public class ErrorHandlerFilter extends OncePerRequestFilter {
+
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
@@ -41,6 +45,27 @@ public class ErrorHandlerFilter extends OncePerRequestFilter {
                 status = HttpStatus.UNAUTHORIZED.value();
             }
             responseWithJson(response, status, toJsonString(failed(e.getResponseCode(), e.getMessage())));
+        }catch (NestedServletException e) {
+            String msg = e.getMessage();
+            log.error(msg, e);
+            Throwable cause = e.getCause();
+
+            int status = HttpStatus.INTERNAL_SERVER_ERROR.value();
+            CommonStatusCode errorCode = CommonStatusCode.FAILED;
+            String errorMsg = msg;
+            if (cause instanceof MaxUploadSizeExceededException) {
+                MaxUploadSizeExceededException maxUploadSizeExceededException = ((MaxUploadSizeExceededException) cause);
+                errorCode = CommonStatusCode.MAX_UPLOAD_SIZE_EXCEEDED_EXCEPTION;
+                String message = maxUploadSizeExceededException.getMessage();
+                if (nonNull(message)) {
+                    errorMsg = message.substring(message.lastIndexOf(":") + 2);
+                }
+                else {
+                    errorMsg = errorCode.getDescr();
+                }
+
+            }
+            responseWithJson(response, status, toJsonString(failed(errorCode, errorMsg)));
         }
         catch (ServletException e) {
             log.error(e.getMessage(),e);
