c4baaef624527005f01ea3a68745990b3c9c907d
ZeroOrInfinity <zyw23zyw23@163.com>
2021-03-22 21:34:47 +0800

    注释: 优化

--- arch-ums/ums-user/src/main/java/org/arch/ums/user/mapper/AddressMapper.java
+++ arch-ums/ums-user/src/main/java/org/arch/ums/user/mapper/AddressMapper.java
@@ -8,8 +8,8 @@ import org.arch.ums.user.entity.Address;
  * 用户地址表(Address) 表数据库 Mapper 层
  *
  * @author YongWu zheng
- * @date 2021-03-17 22:31:29
- * @since 1.0.0
+ * @date   2021-03-17 22:31:29
+ * @since  1.0.0
  */
 @Mapper
 public interface AddressMapper extends CrudMapper<Address> {
