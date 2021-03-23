c34ba5c34c719bff0de2923e90262057dc661f9f
ZeroOrInfinity <zyw23zyw23@163.com>
2021-03-23 21:53:31 +0800

    sso/改进: 获取推荐码 API 返回推荐码直接返回带有推荐码的登录链接.

--- arch-auth/auth-sso/src/main/java/org/arch/auth/sso/controller/RecommendController.java
+++ arch-auth/auth-sso/src/main/java/org/arch/auth/sso/controller/RecommendController.java
@@ -1,17 +1,21 @@
 package org.arch.auth.sso.controller;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.arch.auth.sso.properties.SsoProperties;
 import org.arch.auth.sso.recommend.service.RecommendAndPromotionService;
 import org.arch.framework.beans.Response;
 import org.arch.framework.beans.exception.constant.AuthStatusCode;
 import org.arch.framework.beans.exception.constant.CommonStatusCode;
 import org.arch.framework.ums.bean.TokenInfo;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.dcenter.ums.security.core.auth.properties.ClientProperties;
+import top.dcenter.ums.security.core.oauth.properties.Auth2Properties;
 
 import static java.util.Objects.isNull;
+import static org.springframework.util.StringUtils.hasText;
 
 /**
  * 用户推荐控制器
@@ -22,15 +26,32 @@ import static java.util.Objects.isNull;
 @RestController
 @RequestMapping("/recommend")
 @Slf4j
-@RequiredArgsConstructor
 public class RecommendController {
 
     private final RecommendAndPromotionService recommendAndPromotionService;
+    /**
+     * Http(s)://www.xxx.xxx/contextPaht/loginPath?_from=
+     */
+    private final String registerUrl;
+
+    public RecommendController(RecommendAndPromotionService recommendAndPromotionService,
+                               Environment environment,
+                               Auth2Properties auth2Properties,
+                               ClientProperties clientProperties,
+                               SsoProperties ssoProperties) {
+        this.recommendAndPromotionService = recommendAndPromotionService;
+        String contextPath = environment.getProperty("server.servlet.contextPath");
+        if (!hasText(contextPath)) {
+            contextPath = "";
+        }
+        this.registerUrl = auth2Properties.getDomain() + contextPath + clientProperties.getLoginPage()
+                + "?" + ssoProperties.getSourceParameterName() + "=";
+    }
 
     /**
      * 获取用户推荐码
      * @param token {@link TokenInfo}
-     * @return  返回用户推荐码
+     * @return  返回带有用户推荐码的注册链接
      */
     @GetMapping("/code")
     public Response<String> getRecommendCode(TokenInfo token) {
@@ -38,7 +59,7 @@ public class RecommendController {
             return Response.failed(AuthStatusCode.UNAUTHORIZED);
         }
         try {
-            return Response.success(this.recommendAndPromotionService.generateUserRecommendCode());
+            return Response.success(this.registerUrl + this.recommendAndPromotionService.generateUserRecommendCode());
         }
         catch (Exception e) {
             log.error("生成用户推荐码失败", e);
