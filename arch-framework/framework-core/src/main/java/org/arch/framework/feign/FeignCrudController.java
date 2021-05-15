b5bc402b886daf7111c7f0d745f8595f4082c468
ZeroOrInfinity <zyw23zyw23@163.com>
2021-05-15 21:51:08 +0800

    core/改进: crud 基类改进.

--- arch-framework/framework-core/src/main/java/org/arch/framework/feign/FeignCrudController.java
+++ arch-framework/framework-core/src/main/java/org/arch/framework/feign/FeignCrudController.java
@@ -1,12 +1,10 @@
-package org.arch.framework.crud;
+package org.arch.framework.feign;
 
-import com.baomidou.mybatisplus.extension.activerecord.Model;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.arch.framework.api.crud.BaseSearchDto;
 import org.arch.framework.beans.Response;
-import org.arch.framework.feign.BaseFeignService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import javax.validation.Valid;
 import java.io.Serializable;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.arch.framework.beans.exception.constant.ResponseStatusCode.FAILED;
 
@@ -27,12 +24,13 @@ import static org.arch.framework.beans.exception.constant.ResponseStatusCode.FAI
  * 面向前端的 feign 通用控制器
  * @author YongWu zheng
  * @date 2021-04-16 14:44
- * @param <T>   实体类
+ * @param <DTO> 实体类对应的 {@link BaseSearchDto}
  * @param <ID>  实体类的 id 类型
  * @param <R>   实体类对应的 {@code TRequest}
  * @param <FS>  {@link BaseFeignService} 子类
  */
-public interface FeignCrudController<T extends Model<T>, ID extends Serializable, R, FS extends BaseFeignService<T, ID>> {
+public interface FeignCrudController<DTO, ID extends Serializable, R,
+        FS extends BaseFeignService<DTO, R, ID>> {
 
     Logger log = LoggerFactory.getLogger(FeignCrudController.class);
 
@@ -43,22 +41,14 @@ public interface FeignCrudController<T extends Model<T>, ID extends Serializable
     FS getFeignService();
 
     /**
-     * 获取 T 的空对象.
-     * @return  返回 T 的空对象.
-     */
-    T getEntity();
-
-    /**
      * 保存
-     * @param request     实体类
+     * @param request  实体类对应的 {@code TRequest}
      * @return  {@link Response}
      */
     @PostMapping
-    default Response<T> save(@Valid @RequestBody R request) {
+    default Response<DTO> save(@Valid @RequestBody R request) {
         try {
-            T entity = getEntity();
-            BeanUtils.copyProperties(request, entity);
-            return getFeignService().save(entity);
+            return getFeignService().save(request);
         }
         catch (Exception e) {
             log.error(e.getMessage(),e);
@@ -68,21 +58,13 @@ public interface FeignCrudController<T extends Model<T>, ID extends Serializable
 
     /**
      * 批量保存
-     * @param entityList 实体类列表
+     * @param requestList 实体类对应的 {@code TRequest} 列表
      * @return  {@link Response}
      */
     @PostMapping("/saves")
-    default Response<List<T>> saveAll(@Valid @RequestBody List<R> entityList) {
+    default Response<List<DTO>> saveAll(@Valid @RequestBody List<R> requestList) {
         try {
-            List<T> tList = entityList.stream()
-                                        .map(r -> {
-                                            T entity = getEntity();
-                                            BeanUtils.copyProperties(r, entity);
-                                            return entity;
-                                        })
-                                        .collect(Collectors.toList());
-
-            return getFeignService().saveAll(tList);
+            return getFeignService().saveAll(requestList);
         }
         catch (Exception e) {
             log.error(e.getMessage(),e);
@@ -96,7 +78,7 @@ public interface FeignCrudController<T extends Model<T>, ID extends Serializable
      * @return  {@link Response}
      */
     @GetMapping(path = "/{id:\\d+}")
-    default Response<T> findById(@PathVariable("id") ID id) {
+    default Response<DTO> findById(@PathVariable("id") ID id) {
         try {
             return getFeignService().findById(id);
         }
@@ -112,15 +94,13 @@ public interface FeignCrudController<T extends Model<T>, ID extends Serializable
 
     /**
      * 根据 request 条件查询对象.
-     * @param request   实体类
+     * @param request   实体类对应的 {@code TRequest}
      * @return  {@link Response}
      */
     @GetMapping("/single")
-    default Response<T> findOne(R request) {
+    default Response<DTO> findOne(@Valid R request) {
         try {
-            T entity = getEntity();
-            BeanUtils.copyProperties(request, entity);
-            return getFeignService().findOne(entity);
+            return getFeignService().findOne(request);
         } catch (Exception e) {
             log.error(e.getMessage(),e);
             if (e instanceof IncorrectResultSizeDataAccessException) {
@@ -133,15 +113,13 @@ public interface FeignCrudController<T extends Model<T>, ID extends Serializable
 
     /**
      * 根据 request 条件查询对象列表.
-     * @param request   实体类
+     * @param request   实体类对应的 {@code TRequest}
      * @return  {@link Response}
      */
     @GetMapping("/find")
-    default Response<List<T>> find(R request) {
+    default Response<List<DTO>> find(@Valid R request) {
         try {
-            T entity = getEntity();
-            BeanUtils.copyProperties(request, entity);
-            return getFeignService().find(entity);
+            return getFeignService().find(request);
         }
         catch (Exception e) {
             log.error(e.getMessage(),e);
@@ -154,7 +132,7 @@ public interface FeignCrudController<T extends Model<T>, ID extends Serializable
      * @return  {@link Response}
      */
     @GetMapping("/list")
-    default Response<List<T>> list() {
+    default Response<List<DTO>> list() {
         try {
             return getFeignService().list();
         }
@@ -166,19 +144,17 @@ public interface FeignCrudController<T extends Model<T>, ID extends Serializable
 
     /**
      * 分页查询.
-     * @param request       实体类
+     * @param request       实体类对应的 {@code TRequest}
      * @param pageNumber    第几页
      * @param pageSize      页大小
      * @return  {@link Response}
      */
     @GetMapping(value = "/page/{pageNumber:\\d+}/{pageSize:\\d+}")
-    default Response<Page<T>> page(R request,
+    default Response<Page<DTO>> page(@Valid R request,
                                    @PathVariable(value = "pageNumber") Integer pageNumber,
                                    @PathVariable(value = "pageSize") Integer pageSize) {
         try {
-            T entity = getEntity();
-            BeanUtils.copyProperties(request, entity);
-            return getFeignService().page(entity, pageNumber, pageSize);
+            return getFeignService().page(request, pageNumber, pageSize);
         }
         catch (Exception e) {
             log.error(e.getMessage(),e);
@@ -208,15 +184,13 @@ public interface FeignCrudController<T extends Model<T>, ID extends Serializable
 
     /**
      * 根据 id 更新实体, 直接更新 不为 null 的值.
-     * @param request   实体
+     * @param request   实体类对应的 {@code TRequest}
      * @return  true 表示更新成功
      */
     @PutMapping
     default Response<Boolean> updateById(@Valid @RequestBody R request) {
         try {
-            T entity = getEntity();
-            BeanUtils.copyProperties(request, entity);
-            return getFeignService().updateById(entity);
+            return getFeignService().updateById(request);
         }
         catch (Exception e) {
             log.error(e.getMessage(),e);
@@ -231,15 +205,13 @@ public interface FeignCrudController<T extends Model<T>, ID extends Serializable
     /**
      * 根据 request 条件模糊查询对象; 模糊查询的条件拼接 {@code CONCAT("%", condition ,"%")},
      * 此方法会对不为 null 的 {@link String} 类型的字段都进行模糊查询.
-     * @param request    实体类
+     * @param request    实体类对应的 {@code TRequest}
      * @return  {@link Response}
      */
     @GetMapping("/like")
-    default Response<List<T>> like(R request) {
+    default Response<List<DTO>> like(@Valid R request) {
         try {
-            T entity = getEntity();
-            BeanUtils.copyProperties(request, entity);
-            return getFeignService().like(entity);
+            return getFeignService().like(request);
         }
         catch (Exception e) {
             log.error(e.getMessage(),e);
