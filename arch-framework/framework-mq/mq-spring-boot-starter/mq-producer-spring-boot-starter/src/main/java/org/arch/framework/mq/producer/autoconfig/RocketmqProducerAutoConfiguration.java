6b07b9965a93d42a4814339ebbdc0f3533fda8ca
geeker-lait <lait.zhang@gmail.com>
2021-05-24 18:37:49 +0800

    add mq module

--- arch-framework/framework-mq/mq-spring-boot-starter/mq-producer-spring-boot-starter/src/main/java/org/arch/framework/mq/producer/autoconfig/RocketmqProducerAutoConfiguration.java
+++ arch-framework/framework-mq/mq-spring-boot-starter/mq-producer-spring-boot-starter/src/main/java/org/arch/framework/mq/producer/autoconfig/RocketmqProducerAutoConfiguration.java
@@ -1,9 +1,11 @@
 package org.arch.framework.mq.producer.autoconfig;
 
+import org.arch.framework.mq.producer.processor.RocketmqProducerBeanDefinitionRegistryPostProcessor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-//@Import({RocketmqProducerBeanDefinitionRegistryPostProcessor.class})
+@Import({RocketmqProducerBeanDefinitionRegistryPostProcessor.class})
 public class RocketmqProducerAutoConfiguration {
     public RocketmqProducerAutoConfiguration() {
     }
